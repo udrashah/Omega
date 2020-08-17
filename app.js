41be833e3cc1280b5a5a41ce516c4616c7a733e9
udrashah <69751011+udrashah@users.noreply.github.com>
2020-08-17 15:19:52 +0545

    Update app.js

--- app.js
+++ app.js
@@ -3,15 +3,15 @@
 
 ///Edit Under This only
 
-var firebaseConfig = {
-  apiKey: "api-key",
-  authDomain: "project-id.firebaseapp.com",
-  databaseURL: "https://project-id.firebaseio.com",
-  projectId: "project-id",
-  storageBucket: "project-id.appspot.com",
-  messagingSenderId: "sender-id",
-  appId: "app-id",
-  measurementId: "G-measurement-id",
+const firebaseConfig = {
+  apiKey: "AIzaSyDh3DtevWXKvO4fnmXsn_ZU5QC7jf81Y9Y",
+  authDomain: "promotion-afa18.firebaseapp.com",
+  databaseURL: "https://promotion-afa18.firebaseio.com",
+  projectId: "promotion-afa18",
+  storageBucket: "promotion-afa18.appspot.com",
+  messagingSenderId: "1095261373958",
+  appId: "1:1095261373958:web:0271d4c6db94f461eea587",
+  measurementId: "G-8R00DRM0BC"
 };
 
      
